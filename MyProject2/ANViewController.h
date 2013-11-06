f149a4bfe2eb1cd526c02285f2e3f0b103023964
Admin <admin@Admins-Mac.local>
2013-11-07 00:37:13 +0400

    Initial Commit

---
+++ MyProject2/ANViewController.h
@@ -0,0 +1,13 @@
+//
+//  ANViewController.h
+//  MyProject2
+//
+//  Created by Admin on 07.11.13.
+//  Copyright (c) 2013 ANesko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ANViewController : UIViewController
+
+@end
---
+++ MyProject2/ANViewController.m
@@ -0,0 +1,29 @@
+//
+//  ANViewController.m
+//  MyProject2
+//
+//  Created by Admin on 07.11.13.
+//  Copyright (c) 2013 ANesko. All rights reserved.
+//
+
+#import "ANViewController.h"
+
+@interface ANViewController ()
+
+@end
+
+@implementation ANViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
