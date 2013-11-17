317c4692a8ca72d363f93898f44c550c9a82aa08
Neonilla <aleksa.nesko@gmail.com>
2013-11-17 23:48:18 +0400

    qqq

---
+++ ANTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ANTableViewController.h
+//  MyProject2
+//
+//  Created by Admin on 17.11.13.
+//  Copyright (c) 2013 ANesko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ANTableViewController : UIViewController
+
+@end
